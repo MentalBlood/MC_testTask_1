f07c8d1473a16eed3b77c29c0a7c85793d46be8d
MentalBlood <necheporenko.stepan@yandex.ru>
2020-07-27 21:04:40 +0300

    added list rendering and styles

--- 2/main.js
+++ 2/main.js
@@ -5,7 +5,7 @@ function generateList(numberOfElements, depth, textPrefix) {
     for (let elementNumber = 0; elementNumber < numberOfElements; elementNumber++) {
         const printableNumber = textPrefix + (elementNumber+1).toString();
         const element = {
-            text: printableNumber + " ->",
+            text: printableNumber + ' ->',
             click: 0,
             next: generateList(numberOfElements, depth - 1, printableNumber + '.')
         }
@@ -14,4 +14,20 @@ function generateList(numberOfElements, depth, textPrefix) {
     return list;
 }
 
-//const list = generateList(10, 3, '');
+const list = generateList(10, 3, '');
+
+function renderList(target, list, listClasses) {
+    const listObject = document.createElement('ul');
+    listClasses.forEach(listClass => listObject.classList.add(listClass));
+    for (const element of list) {
+        const elementObject = document.createElement('li');
+        const elementButton = document.createElement('button');
+        elementButton.innerText = '(' + element.click.toString() + ') ' + element.text;
+        elementObject.appendChild(elementButton);
+        listObject.appendChild(elementObject);
+    }
+    target.appendChild(listObject);
+}
+
+const target = document.querySelector('.root');
+renderList(target, list, ['list']);
