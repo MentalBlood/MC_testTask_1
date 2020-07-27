d5402a49ca47d7ad0a210beb40d7dcd5835619b2
MentalBlood <necheporenko.stepan@yandex.ru>
2020-07-27 22:15:54 +0300

    added pause button function

--- 3/main.js
+++ 3/main.js
@@ -1,13 +1,17 @@
 const timeObject = document.querySelector('.time');
+timeObject.innerText = '0.00';
 
 let intervalId = undefined;
 let startTime = undefined;
 function start() {
-    startTime = new Date();
+    startTime = Date.now() - Number.parseFloat(timeObject.innerText) * 1000;
     intervalId = setInterval(() => {
-        const currentTime = new Date();
+        const currentTime = Date.now();
         timeObject.innerText = ((currentTime - startTime) / 1000).toFixed(2);
     }, 10);
+}
+function pause() {
+    clearInterval(intervalId);
 }
 function stop() {
     clearInterval(intervalId);
@@ -16,6 +20,8 @@ function stop() {
 
 const startButton = document.querySelector('.startButton');
 startButton.onclick = start;
+const pauseButton = document.querySelector('.pauseButton');
+pauseButton.onclick = pause;
 const stopButton = document.querySelector('.stopButton');
 stopButton.onclick = stop;
 
