2863e60233b6eaf5602ccb7a2a2bd625d2e02c8d
MentalBlood <necheporenko.stepan@yandex.ru>
2020-07-27 21:46:32 +0300

    added buttons functions

--- 2/main.js
+++ 2/main.js
@@ -2,8 +2,8 @@ function generateList(numberOfElements, depth, textPrefix) {
     const list = [];
     if (depth < 0)
         return list;
-    for (let elementNumber = 0; elementNumber < numberOfElements; elementNumber++) {
-        const printableNumber = textPrefix + (elementNumber+1).toString();
+    for (let elementIndex = 0; elementIndex < numberOfElements; elementIndex++) {
+        const printableNumber = textPrefix + (elementIndex+1).toString();
         const element = {
             text: printableNumber + ' ->',
             click: 0,
@@ -14,20 +14,48 @@ function generateList(numberOfElements, depth, textPrefix) {
     return list;
 }
 
-const list = generateList(10, 3, '');
+const rootList = generateList(10, 3, '');
 
-function renderList(target, list, listClasses) {
+function renderList(target, list, listClasses = []) {
+    target.innerHTML = '';
     const listObject = document.createElement('ul');
     listClasses.forEach(listClass => listObject.classList.add(listClass));
-    for (const element of list) {
+    if (list !== rootList) {
+        const backButton = document.createElement('button');
+        backButton.innerText = 'back';
+        backButton.onclick = function() {
+            path.pop();
+            render();
+        }
+        target.appendChild(backButton);
+    }
+    for (let elementIndex = 0; elementIndex < list.length; elementIndex++) {
+        const element = list[elementIndex];
         const elementObject = document.createElement('li');
         const elementButton = document.createElement('button');
         elementButton.innerText = '(' + element.click.toString() + ') ' + element.text;
+        elementButton.onclick = function() {
+            element.click += 1;
+            path.push(elementIndex);
+            render();
+        }
         elementObject.appendChild(elementButton);
         listObject.appendChild(elementObject);
     }
     target.appendChild(listObject);
 }
 
+const path = [];
+function getCurrentList() {
+    let list = rootList;
+    for (const index of path)
+        list = list[index].next;
+    return list;
+}
+
 const target = document.querySelector('.root');
-renderList(target, list, ['list']);
+function render() {
+    renderList(target, getCurrentList(), ['list']);
+}
+
+render();
