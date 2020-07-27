58dfb8172a50841e79fa706a0438ed5100743e13
MentalBlood <necheporenko.stepan@yandex.ru>
2020-07-27 22:24:56 +0300

    added task 4

---
+++ 4/main.js
@@ -0,0 +1,18 @@
+var _x = 1;
+
+var foo = {
+    _x: 2,
+    bar: function() {
+        return this._x;
+    }
+}
+
+console.log('1. ', foo.bar());
+var baz = foo.bar;
+console.log('2. ', baz.bind(foo)());
+
+setTimeout(function() {
+    console.log('3. ', baz());
+}, 0);
+
+var _x = 4;
